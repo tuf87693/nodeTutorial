08f26ce81b2a83d0f623a5958cf8540dd57c1030
tuf87693 <tuf87693@temple.edu>
2015-04-01 17:45:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
-	res.sendfile('./public/index.html');
+	res.send('hello world');
 });
 
 app.listen(app.get('port'), function() {
